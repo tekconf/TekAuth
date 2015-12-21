94646c8f8034e5562070f2af25547b8b9a8bb31b
Rob Gibbens <RobGibbens@gmail.com>
2015-12-21 13:01:20 -0500

    trying grouping by stack

--- mobile/ios/StackCell.designer.cs
+++ mobile/ios/StackCell.designer.cs
@@ -14,8 +14,48 @@ namespace ios
 	[Register ("StackCell")]
 	partial class StackCell
 	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel sessionDescription { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel sessionRoom { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel sessionSpeaker { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel sessionTime { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel sessionTitle { get; set; }
+
 		void ReleaseDesignerOutlets ()
 		{
+			if (sessionDescription != null) {
+				sessionDescription.Dispose ();
+				sessionDescription = null;
+			}
+			if (sessionRoom != null) {
+				sessionRoom.Dispose ();
+				sessionRoom = null;
+			}
+			if (sessionSpeaker != null) {
+				sessionSpeaker.Dispose ();
+				sessionSpeaker = null;
+			}
+			if (sessionTime != null) {
+				sessionTime.Dispose ();
+				sessionTime = null;
+			}
+			if (sessionTitle != null) {
+				sessionTitle.Dispose ();
+				sessionTitle = null;
+			}
 		}
 	}
 }
