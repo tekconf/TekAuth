454a8531f458593605528bf92170309efb6f3ff2
Rob Gibbens <RobGibbens@gmail.com>
2015-12-23 00:46:21 -0500

    Added Session Detail VC

---
+++ mobile/ios/SessionDetailViewController.cs
@@ -0,0 +1,14 @@
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace ios
+{
+	partial class SessionDetailViewController : UIViewController
+	{
+		public SessionDetailViewController (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
