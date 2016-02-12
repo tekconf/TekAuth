e37d277a500277e87dc51537a28e349a24e9adbb
Rob Gibbens <RobGibbens@gmail.com>
2016-02-12 00:21:01 -0500

    Kick build

--- mobile/ios/AppDelegate.cs
+++ mobile/ios/AppDelegate.cs
@@ -14,8 +14,6 @@ using TekConf.Mobile.Core.Messages;
 
 namespace ios
 {
-	// The UIApplicationDelegate for the application. This class is responsible for launching the
-	// User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
 	[Register ("AppDelegate")]
 	public class AppDelegate : UIApplicationDelegate
 	{
